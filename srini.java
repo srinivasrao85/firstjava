0bf935732244aec5a6a28c7595489916c4b39247
srini <srini@google.com>
2023-06-05 14:52:48 +0000

    Monday

--- srini.java
+++ srini.java
@@ -3,7 +3,7 @@
 class HelloWorld {
     public static void main(String[] args) {
          System.out.println("Hello,World");
-         System.out.println("Welcome Git!!!");  
+         System.out.println("Welcome from local Git mannn!!!");  
     }
 
 }
